2ade29cdbd01c9c1eab4de4e0c3a606d33787c9a
Robert Jakob <rj@robertjakob.de>
2014-08-11 16:21:22 +0200

    JSON integration.

--- src/PMRSAbs.hs
+++ src/PMRSAbs.hs
@@ -1,30 +1,46 @@
-import Control.Monad (when)
+import Control.Monad (when, forM_)
 import Data.Maybe (isNothing, fromJust)
+import Data.List (intercalate)
+import System.TimeIt
+import System.Environment
+import Text.Printf (printf)
 
 import           Abstraction ()
 import           Examples (exampleDetWpmrs)
 import           PMRS ()
 import           Sorts ()
 import           Term ()
+import Options
 --import           PMRSParser
 import Preface as P
 import HORS ()
 import Transformation
 import GenericRecursionScheme ()
 
+
 main :: IO ()
 main = do
+	(args, _files) <- getArgs >>= pmrsabsOptions
+	let prefaceDir = getPrefaceDir args
 	putStrLn "PMRS Modelchecker Version 1.0"
-	x <- P.version "Preface.exe"
+	x <- P.version prefaceDir
 	when (isNothing x) (error "Preface.exe not found")
-	putStrLn $ "- Preface version: " ++ fromJust x
+	putStrLn $ "- Preface version: " ++ (intercalate "." $ map show $ fromJust x)
+	when (not $ versionCheck [1,3] (fromJust x)) $ error "Preface version of at least 1.3 required."
 	--
-	rsfd <- exampleDetWpmrs >>= wPMRStoRSFD
-	putStrLn $ "- Transforming to RSFD"
+	putStr $   "- Transforming to RSFD"
+	(rsfdTime, rsfd) <- timeItT $ exampleDetWpmrs >>= wPMRStoRSFD
+	putStrLn $ printf " (%6.4fs)" rsfdTime
+
 	--
-	putStrLn $ "- Calling Preface"
+	putStr $ "- Calling Preface"
+	(checkTime, result) <- timeItT $ check prefaceDir rsfd ATT
+	putStrLn $ printf " (%6.4fs)" checkTime
 	--
-	result <- check rsfd ATT
 	case result of
 		Left e -> putStrLn $ "Error running Preface: " ++ show e
-		Right e -> putStrLn e
+		Right e -> do
+			putStr "- Accepted: "
+			let res = problemResult e
+			print $ res
+			when (not res) $ putStr "- Certificate: " >> forM_ (problemCertificate e) print
--- src/Preface.hs
+++ src/Preface.hs
@@ -1,18 +1,34 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 
 module Preface (
-	ATT(..), Answer(..),
-	check,
-	version
+  ATT(..), Answer(..),
+  check,
+  version,
+  versionCheck,
+  PrefaceOutput(..),
+  problemResult,
+  problemCertificate,
+  SchemeData(..),
+  AutomatonData(..),
+  ProblemData(..),
+  PerformanceData(..),
+  CertificateData(..)
 ) where
 
 import Control.Exception (try, IOException, catch)
 -- import Control.Concurrent (threadDelay)
-import Control.Monad (liftM)
+import Control.Monad (liftM, mzero)
+import Control.Applicative ((<$>),(<*>))
+import Control.Arrow (second)
+import Data.Aeson (decode, Value(..), FromJSON(..),(.:),(.:?))
+import Data.ByteString.Lazy.Char8 (pack)
+import Data.Maybe (fromJust)
+import qualified Data.HashMap.Strict as HM
 import System.IO
 import System.IO.Temp
 import System.Process (readProcess)
 import Text.Regex (mkRegex, matchRegex)
+import qualified Data.Text as T
 
 import Aux
 import PMRS
@@ -22,7 +38,7 @@ import Term
 data ATT = ATT
 
 instance PrettyPrint ATT where
-	prettyPrint _ = "%BEGINA\nq0 cons-> q1 q0.\nq1 s -> q2.\nq2 z ->.%ENDA\n"
+  prettyPrint _ = "%BEGINA\nq0 cons-> q1 q0.\nq1 s -> q2.\nq2 a ->.%ENDA\n"
 
 class PrettyPrint a => PrefaceGrammar a where
 
@@ -32,34 +48,126 @@ instance PrefaceGrammar RSFD where
 --prefacePath :: String
 --prefacePath = "/home/jakobro/work/impl/PrefaceModelchecker/Preface/Preface/bin/Debug/Preface.exe"
 
-prefaceFileName :: String
-prefaceFileName = "Preface.exe"
-
 data Answer = Accepted
             | Rejected Term
 
 -- | Calls Preface which checks whether the given ATT accepts
 -- the tree produced by the grammar and returns.
-check :: PrefaceGrammar a => a -> ATT -> IO (Either IOError String)
-check grammar att =
-	withSystemTempFile "preface.input" $ \path h -> do
-		hPutStr h input
-		hFlush h
-		catch (liftM Right $ readProcess prefaceFileName [path] "") $ \(e :: IOError) -> return $ Left e
+check :: PrefaceGrammar a => FilePath -> a -> ATT -> IO (Either IOError PrefaceOutput)
+check prefaceDir grammar att =
+  withSystemTempFile "preface.input" $ \path h -> do
+    hPutStr h input
+    hFlush h
+    res <- catch (liftM Right $ readProcess prefaceDir ["--json", "--witness", "-c", path] "") $ \(e :: IOError) -> return $ Left e
+    return $ fromJust <$> parsePrefaceOutput <$> res
   where
-		input = prettyPrint grammar ++ "\n" ++ prettyPrint att
+    input = prettyPrint grammar ++ "\n" ++ prettyPrint att
 
 -- | Extract the preface version string.
-version :: FilePath -> IO (Maybe String)
+version :: FilePath -> IO (Maybe [Int])
 version preface = do
-	res <- try (readProcess preface ["-v"] "") :: IO (Either IOException String)
-	case res of
-		Left  _ -> return Nothing
-		Right s -> return $ extractVersion s
+  res <- try (readProcess preface ["-v"] "") :: IO (Either IOException String)
+  case res of
+    Left  _ -> return Nothing
+    Right s -> return $ extractVersion s
 
 -- | Tries to extract a version string of the form "Version 1.0"
 -- from a given string.
-extractVersion :: String -> Maybe String
+extractVersion :: String -> Maybe [Int]
 extractVersion s = do
-	x <- matchRegex (mkRegex "Version: ([[:digit:]]\\.[[:digit:]])") s
-	return $ head x
+  x <- matchRegex (mkRegex "Version: ([[:digit:]])\\.([[:digit:]])") s
+  return $ map read x
+
+-- | Checks if a given version is at least the same as the
+-- required version.
+versionCheck :: [Int] -> [Int] -> Bool
+versionCheck expversion foundversion= all id res
+  where
+    res = zipWith (>=) foundversion expversion
+
+
+-- * Output Parser
+
+data PrefaceOutput = PrefaceOutput
+  { poVersion :: T.Text
+  , poScheme :: SchemeData
+  , poAutomaton :: AutomatonData
+  , poProblem :: ProblemData
+  , poPerformance :: PerformanceData
+  , poCertificate :: Maybe CertificateData
+  } deriving Show
+
+problemResult :: PrefaceOutput -> Bool
+problemResult po =
+  case poDecision $ poProblem po of
+    "Accepted" -> True
+    "Rejected" -> False
+
+problemCertificate :: PrefaceOutput -> [(T.Text, T.Text)]
+problemCertificate po = cdTypes $ fromJust $ poCertificate po
+
+data SchemeData = SchemeData
+  { sdRules :: T.Text -- TODO Int
+  , sdOrder :: T.Text
+  , sdArity :: T.Text
+  , sdSize  :: T.Text
+  } deriving Show
+
+data AutomatonData = AutomatonData
+  { adStates :: T.Text
+  , adTransitions :: T.Text
+  } deriving Show
+
+data ProblemData = ProblemData
+  { poDecision :: T.Text
+  } deriving Show
+
+data PerformanceData = PerformanceData
+  { poTime :: T.Text
+  } deriving Show
+
+data CertificateData = CertificateData
+  { cdTypes :: [(T.Text, T.Text)]
+  } deriving Show
+
+instance FromJSON PrefaceOutput where
+  parseJSON (Object v) = PrefaceOutput <$>
+                         v .: "Version" <*>
+                         v .: "Scheme" <*>
+                         v .: "Automaton" <*>
+                         v .: "Problem" <*>
+                         v .: "Performance" <*>
+                         v .:? "Certificate"
+  parseJSON _ = mzero
+
+instance FromJSON SchemeData where
+  parseJSON (Object v) = SchemeData <$>
+                         v .: "Rules" <*>
+                         v .: "Order" <*>
+                         v .: "Arity" <*>
+                         v .: "Size"
+  parseJSON _ = mzero
+
+instance FromJSON AutomatonData where
+  parseJSON (Object v) = AutomatonData <$>
+                         v .: "States" <*>
+                         v .: "Transitions"
+  parseJSON _ = mzero
+
+instance FromJSON ProblemData where
+  parseJSON (Object v) = ProblemData <$>
+                         v .: "Decision"
+  parseJSON _ = mzero
+
+instance FromJSON PerformanceData where
+  parseJSON (Object _) = return $ PerformanceData "" -- TODO
+  parseJSON _ = mzero
+
+instance FromJSON CertificateData where
+  parseJSON (Object v) = do
+    let valueToString (String txt) = txt
+    return $ CertificateData $ map (second valueToString) $ HM.toList v
+  parseJSON _ = mzero
+
+parsePrefaceOutput :: String -> Maybe PrefaceOutput
+parsePrefaceOutput s = decode $ pack s
